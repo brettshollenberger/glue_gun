00bf49429688a143b7c8d0c9bcea1ef8706a0a2d
Brett Shollenberger <brettshollenberger@Bretts-MacBook-Pro-2.local>
2024-10-04 08:47:29 -0400

    0.1.4

--- lib/glue_gun/types.rb
+++ lib/glue_gun/types.rb
@@ -1,9 +1,9 @@
 module GlueGun
   module Types
-    require_relative "glue_gun/types/hash_type"
+    require_relative "types/hash_type"
     ActiveModel::Type.register(:hash, GlueGun::Types::HashType)
 
-    require_relative "glue_gun/types/array_type"
+    require_relative "types/array_type"
     ActiveModel::Type.register(:array, GlueGun::Types::ArrayType)
   end
 end
